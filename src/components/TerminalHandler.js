695c9570ac8eb5a36e45f41e14672954d8257aa2
Nygosaki <63132234+Nygosaki@users.noreply.github.com>
2023-11-16 11:10:16 +0100

    added intro to top

--- src/components/TerminalHandler.js
+++ src/components/TerminalHandler.js
@@ -34,7 +34,49 @@ function TerminalHandler() {
 
     return (
         <div>
+          <div className='terminal'>
+          <div className='horizontalAlign'>
+          <p>+<br />|<br />|<br />|<br />|<br />|<br />|<br />|<br />|<br />|<br />|<br />|<br />|<br />|<br />+</p>
+          <div className='intro'>
+            <p>---------------------------------------------------------------------------</p>
+            <div className='horizontalAlign'>
+              <img src={require("../assets/pfp.png")} style={{maxWidth:"8vw", paddingLeft:"1vw"}} alt=''/>
+              <p style={{paddingLeft:"1vw"}}><strong>Hey there!</strong><br />
+              My name's <strong>Nygosaki</strong> <span class="descriptor">(She/Any)</span><br />
+              I am a programmer, also interasted in<br />
+              general IT, cybersecurity and pentesting.<br />
+              Currently focusing on bots and other 'malicious' programs.<br />
+              Commissions are open btw :3<br />
+              </p>
+              </div>
+              <div className='horizontalAlign'>
+              <a href='https://github.com/Nygosaki'>
+              <img src={require("../assets/icoGithub.png")} className='iconSocial' style={{paddingLeft:"10vw"}} alt='Github'/>
+              </a>
+              <a href='https://discord.gg/sFzqJk9R7E'>
+              <img src={require("../assets/icoDiscord.png")} className='iconSocial' alt='Discord'/>
+              </a>
+              <a href='https://www.last.fm/user/MeLikeFish'>
+              <img src={require("../assets/icoLastfm.png")} className='iconSocial' alt='Last.fm'/>
+              </a>
+              <a href='https://www.tumblr.com/blog/melikefish'>
+              <img src={require("../assets/icoTumblr.png")} className='iconSocial' alt='Tumblr'/>
+              </a>
+              <a href='https://steamcommunity.com/profiles/76561198433226986'>
+              <img src={require("../assets/icoSteam.png")} className='iconSocial' alt='Discord'/>
+              </a>
+              </div>
+              <p>---------------------------------------------------------------------------<br /></p>
+              <div className='horizontalAlign'>
+              <p style={{paddingLeft:'10vw'}}>This is a manually emulated interactive CLI.<br />
+              You should run the `help` command to get started :3</p>
+              </div>
+              <p>---------------------------------------------------------------------------</p>
+            </div>
+            <p>+<br />|<br />|<br />|<br />|<br />|<br />|<br />|<br />|<br />|<br />|<br />|<br />|<br />|<br />+</p>
+            </div>
             <div className="terminals-container">{terminals}</div>
+        </div>
         </div>
     )
 };
