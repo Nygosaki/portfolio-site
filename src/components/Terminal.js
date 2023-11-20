25e34a4ae2b37cf1256e87b1333d59e22d60dc2a
Nygosaki <63132234+Nygosaki@users.noreply.github.com>
2023-11-20 13:24:37 +0100

    Added commands

--- src/components/Terminal.js
+++ src/components/Terminal.js
@@ -14,24 +14,31 @@ function Terminal({ searchParams }) {
       console.log(history)
       switch (inputCommand.split(' ')[0]) {
           case 'help':
-            setOutput("Hey there. Sorry, but the commands are still under construction. Thank you for helping me test everything :>");
+            setOutput('<p>Hey there. Sorry, but the commands are still under construction. Thank you for helping me test everything :><br />help - get the current list of commands with secrets hidden<br />commissions - see info about my programming commissions<br />neofetch - generate a neofetch of the server<br />credits - see who helped in the creation of this website<br />cuteimage - check out this cute image :3<br /><3');
             break;
           case 'credits':
-              setOutput("Thanks to my beutiful girlfriend, UwU (the beatsaber player) for helping me with some CSS :>");
-              break;
+            setOutput(`
+            <div class="horizontalAlign">
+              <img src="ashley.jpeg" class='credits'/>
+              <p>Thanks to:<br />My beutiful girlfriend, <a href=https://github.com/Ash-does-stuff>Ash-does-stuff</a><br />For helping with some css :></p>
+            </div>
+            `);   
+            document.title = "Love you Ashley<3";       
+            break;
           case 'commissions':
-              setOutput("If you want to comission some app (or most likely botting tools), my rates are $20 per hour of work (though I am leniant with what I call an hour of work). Contact me on Discord for further details.");
+              setOutput("<p>If you want to comission some app (or most likely botting tools), my rates are $20 per hour of work (though I am leniant with what I call an hour of work). <a href=https://discord.gg/sFzqJk9R7E>Contact me on Discord</a> for further details.</p>");
               break;
           case 'neofetch':
             setOutput('<p>       _,met$$$$$gg.</p><p>    ,g$$$$$$$$$$$$$$$P.</p><p>  ,g$$P"     """Y$$.".</p><p> ,$$P\'              `$$$.</p><p>\',$$P       ,ggs.     `$$b:   <span class="neofetchtext">' + searchParams.get("user") + '</span>@<span class="neofetchtext">Server2</span></p><p>`d$$\'     ,$P"\'   <span class="neofetchtext">.</span>    $$$    --------------------------------</p><p> $$P      d$\'     <span class="neofetchtext">,</span>    $$P    <span class="neofetchtext">OS</span>: Debian GNU/Linux 11 (bullseye) x86_64</p><p> $$:      $$.   <span class="neofetchtext">-</span>    ,d$$\'    <span class="neofetchtext">Host</span>: Google Compute Engine</p><p> $$;      Y$b._   _,d$P\'      <span class="neofetchtext">Kernel</span>: 6.1.42+</p><p> Y$$.    <span class="neofetchtext">`.</span>`"Y$$$$P"\'         <span class="neofetchtext">Uptime</span>: 1+ years</p><p> `$$b      <span class="neofetchtext">"-.__</span>              <span class="neofetchtext">Packages</span>: 733 (dpkg)</p><p>  `Y$$                        <span class="neofetchtext">Shell</span>: bash 5.1.4</p><p>   `Y$$.                      <span class="neofetchtext">Terminal</span>: /dev/pts/1</p><p>     `$$b.                    <span class="neofetchtext">CPU</span>: Intel Xeon (4) @ 2.199GHz</p><p>       `Y$$b.                 <span class="neofetchtext">Memory</span>: ' + (Math.floor(Math.random() * (16002 - 300 + 1)) + 300) + 'MiB / 16002MiB</p><p>          `"Y$b._</p><p>              `"""                                    </p><p>                                                      </p>');
             document.title = "Oh Bread!"
             break;
-          case 'cute':
-            setOutput("<p>OOO THATS RIGHT, YOU ARE MY KUTE LITTELE KITTEN :3</p>");
-            document.title = "Love you Ashley<3";
-            break;
           case 'intro':
-            setOutput('')
+            setOutput(`Easteregg!`);       
+            break;
+          case 'cuteimage':
+            setOutput(`
+              <img src="coupleArt.jpg" class='largeImage'/>
+          `);  
             break;
           default:
               setOutput('<p>Invalid command: ' + inputCommand + '</p><p>Please try running the `help` command</p>');
