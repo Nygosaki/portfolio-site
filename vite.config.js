4ce7e1b6c700db773661e951fcb6f5c98c88adc5
Nygosaki <63132234+Nygosaki@users.noreply.github.com>
2024-12-17 17:20:44 +0100

    fixing non-functioning index file in long paths

--- vite.config.js
+++ vite.config.js
@@ -134,7 +134,7 @@ function basePlugin() {
         config(_, { mode }) {
             const { PUBLIC_URL } = loadEnv(mode, ".", ["PUBLIC_URL"]);
             return {
-                base: PUBLIC_URL || "",
+                base: PUBLIC_URL || "/",
             };
         },
     };
